c919c659524403acf098639a9de12aa58e73928f
Hyunrae Kim <33170895+hyunraekim@users.noreply.github.com>
2017-12-10 16:42:53 -0600

    Update index.js
    Bug fixes for publishing requirements

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 
-//Messages
+//MESSAGES
 
-const WELCOME_MESSAGE = "Welcome to three six nine. You can play three six nine with just me or with friends. Say help for how to play";
-
+const WELCOME_MESSAGE = "Welcome to three six nine. You can play three six nine with just me or with friends. Examples are play, play with three people, or help for how to play.";
 const START_GAME_MESSAGE = "OK. I will start!";
-
-const EXIT_SKILL_MESSAGE = "Better luck next time!";
-
-const HELP_MESSAGE = "Three six nine is a game where we take turns counting up from one. If the number is divisible by three, you're going to say quack. If the number has a three, six, or nine anywhere, you're going to say quack";
-
+const HELP_MESSAGE = "Three six nine is a game where we take turns counting up from one. If the number is divisible by three, you're going to say quack. If the number has a three, six, or nine anywhere, you're going to say quack. If the number contains more than one digit that contains either a three, six, or nine, then you're going to say quack quack";
+const exitSkillMessage = ["Better luck next time!", "You'll get it next time!", "Good effort", "See you again", "You'll get better, don't worry"];
 const speechConsWrong = ["Argh", "Aw man", "Blarg", "Blast", "Boo", "Bummer", "Darn", "D'oh", "Dun dun dun", "Eek", "Honk", "Le sigh",
 "Mamma mia", "Oh boy", "Oh dear", "Oof", "Ouch", "Ruh roh", "Shucks", "Uh oh", "Wah wah", "Whoops a daisy", "Yikes"];
 
@@ -19,9 +15,12 @@ const states = {
     GAME: "_GAME"
 };
 
-//Game Variables
+//GAME VARIABLES
+
 var counter = 1; 
 var numPlayers = 1; //By default is one
+
+//HANDLERS 
 
 const handlers = {
     //Game goes straight to play currently 
@@ -62,17 +61,22 @@ const startHandlers = Alexa.CreateStateHandler(states.START,{
         this.emitWithState("Game");
     },
      "PlayWithFriends": function() { 
-         const itemSlot = this.event.request.intent.slots.Item;
-         numPlayers = itemSlot; //set number to slot value
+         const itemSlot = this.event.request.intent.slots.numFriends.value; 
+         numPlayers = itemSlot;
+         numPlayers++; //account for Alexa
          this.handler.state = states.GAME; 
          this.emitWithState("Game"); 
     },
     "AMAZON.StopIntent": function() {
-        this.response.speak(EXIT_SKILL_MESSAGE);
+        let exitMessage = getExitSkillMessage();
+        this.response.speak(exitMessage);
+        endGame();
         this.emit(":responseReady");
     },
     "AMAZON.CancelIntent": function() {
-        this.response.speak(EXIT_SKILL_MESSAGE);
+        let exitMessage = getExitSkillMessage();
+        this.response.speak(exitMessage);
+        endGame(); 
         this.emit(":responseReady");
     },
     "AMAZON.HelpIntent": function() {
@@ -127,12 +131,14 @@ const gameHandlers = Alexa.CreateStateHandler(states.GAME,{
         }
     },
     "AMAZON.StopIntent": function() {
-        this.response.speak(EXIT_SKILL_MESSAGE);
+        let exitMessage = getExitSkillMessage();
+        this.response.speak(exitMessage);
         endGame();
         this.emit(":responseReady");
     },
     "AMAZON.CancelIntent": function() {
-        this.response.speak(EXIT_SKILL_MESSAGE);
+        let exitMessage = getExitSkillMessage();
+        this.response.speak(exitMessage);
         endGame();
         this.emit(":responseReady");
     },
@@ -141,10 +147,14 @@ const gameHandlers = Alexa.CreateStateHandler(states.GAME,{
         this.emit(":responseReady");
     },
     "Unhandled": function() {
-        this.emitWithState("Game");
+        this.emit(":ask", "test test");
     }
 });
 
+
+//FUNCTIONS
+
+//Returns whether the inputted answer is correct (User)  
 function checkAnswer(slots, value)
 {
         for (var slot in slots) {  
@@ -153,12 +163,14 @@ function checkAnswer(slots, value)
             let counterValue = value.toString();
             let divisible = value % 3; 
             return (slotValue == checkQuack(counterValue, divisible));
-        }
+        } 
 }
     return false;
 }
-
+ 
+//Returns what the correct input/output should be according to game rules (Alexa and User)
 function checkQuack(counterString, divisible) { 
+    //counter for how many times three, six, nine occurs in the digits of counterString
     let count = 0; 
     while (counterString.indexOf('3') > -1 || counterString.indexOf('6') > -1 || counterString.indexOf('9') > -1) { 
         if (counterString.indexOf('3') > - 1) { 
@@ -204,23 +216,34 @@ function checkQuack(counterString, divisible) {
     }
 }
 
+//when the game ends, reset variables, and return message. 
 function endGame() { 
     let speechOutput = "";
     let response = getSpeechCon(false);
     response += "your final score is " + counter;
-    speechOutput = response + ". " + EXIT_SKILL_MESSAGE;
+    let exitMessage = getExitSkillMessage();
+    speechOutput = response + ". " + exitMessage;
+    //Reset variables here 
     counter = 1; 
     numPlayers = 1; 
     return speechOutput; 
 }
 
+//Get randomized lost message
 function getSpeechCon(type) {
     return "<say-as interpret-as='interjection'>" + speechConsWrong[getRandom(0, speechConsWrong.length-1)] + " </say-as><break strength='strong'/>";
 }
 
+function getExitSkillMessage() { 
+    return exitSkillMessage[getRandom(0, exitSkillMessage.length-1)]; 
+}
+
+//Get random for getSpeechCon function
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max-min+1)+min);
 }
+
+
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
